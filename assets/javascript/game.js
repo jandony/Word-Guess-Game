05e9c979dca9ef0d1286a67320e8ec4729333cce
Jeff Andony <jeff.andony@gmail.com>
2019-06-02 10:45:52 -0600

    Fixed syntax errors and mispelled words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,99 @@
-// Set all Global variables
-var totalWins = 0;
-var guesses = 10;
-var dashAmount = 0; // number of dashes (letters)
-var displayDash = []; // dashes to be displayed
-var wordLetters = []; //splits each name into its own array
-var guessedLetters = []; // guessed letters needs to match wordLetters
+// Step 1 - Set all Global variables
+    var totalWins = 0;
+    var guesses = 10;
+    var dashAmount = 0; // number of dashes (letters)
+    var displayDash = []; // dashes to be displayed
+    var wordArray = []; //splits each name into its own array - Array 1
+    var letterGuessArray = []; // letterGuessArray needs to match wordArray - Array 2
 
-userGuess = event.key;
+    // Set all Game Choices as variables or arrays
+        var superHeroes = ["hulk", "spiderman", "superman", "batman", "thor"]
 
-// Set all Game Choices as variables or arrays
-var superHeroes = ["hulk", "spiderman", "superman", "batman", "thor"]
-
-// Randomize the computer's guess
-var computerGuess = superHeroes[Math.floor(Math.random() * superHeroes.length)];
-
-// Set all all HTML sections we want to update (making shortcuts)
-var winsText = document.getElementById("wins");
-var currentWordText = document.getElementById("currentWord");
-var guessesText = document.getElementById("guessesRemaining");
-var letterGuessedText = document.getElementById("letterGuessed");
-
-// Step 1 - Set all Global functions
-function gameSetUp() {
-    // Set random variable for all superHeroes 
+    // Set variable to select a random superhero
     var superHero = superHeroes[Math.floor(Math.random() * superHeroes.length)];
 
-    // Set each array value into its own array
-    var wordLetters = superHero.split("");
+    // Set all HTML sections we want to update (making shortcuts)
+        var winsText = document.getElementById("wins");
+        var currentWordText = document.getElementById("currentWord");
+        var guessesText = document.getElementById("guessesRemaining");
+        var letterGuessedText = document.getElementById("letterGuessed");
 
-    // Set a variable to match the dashes to amount of letters
-    dashAmount = wordLetters.length;
 
-    // Create a for loop for the amount of dashes per array value
-    for (var i = 0; i < dashes; i++) {
-        displayDash.push("_");
-    }
-    currentWordText.innerHTML = " " + displayDash.join(" ");
 
-}
+// Step 2 - Set all Global functions
+    function gameSetUp() {
+        // Set variable to select a random superhero
+        superHero = superHeroes[Math.floor(Math.random() * superHeroes.length)];
 
-function reset() {
-    // updating the HTML to reflect the new variable values
-    gameSetUp();
-    guessedLetters = [];
-    displayDash = [];
-    guesses = 10;
-}
+        // Set each array value into its own array
+        var wordArray = superHero.split("");
 
-function letterValidate (letter) {
-    var lettersIncluded = false;
+        // Set a variable to match the dashes to amount of letters
+        dashAmount = wordArray.length;
 
-    for (var i = 0; i < dashAmount; i++) {
-        if (superHero[i] == letter) {
-            lettersIncluded = true;
-        }
-    }
-
-    if (lettersIncluded) {
+        // Create a for loop for the amount of dashes per array value
         for (var i = 0; i < dashAmount; i++) {
-            if(superhero[i] == letter) {
-                dashAmount == letter;
+            displayDash.push("_");
+        }
+        currentWordText.textContent = " " + displayDash.join(" ");
+
+    }
+
+    function reset() {
+        gameSetUp();
+        letterGuessArray = [];
+        displayDash = [];
+        guesses = 10;
+    }
+
+    function letterValidate(letter) {
+        var lettersIncluded = false;
+
+        for (var i = 0; i < dashAmount; i++) {
+            if (superHero[i] == letter) {
+                lettersIncluded = true;
             }
         }
-    } else {
-        guesses--;
-        letterGuessedText.push(letterGuessed)
+
+        if (lettersIncluded) {
+            for (var i = 0; i < dashAmount; i++) {
+                if (superHero[i] == letter) {
+                    dashAmount == letter;
+                }
+            }
+        } else {
+            guesses--;
+            letterGuessedText.push("letterGuessed")
+        }
     }
-}
+
+    function result() {
+        if (wordArray === letterGuessArray) {
+            totalWins++;
+            reset();
+        }
+
+        if (guesses = 0) {
+            reset();
+        }
+    }
+
+// Call the Game Setup function
+gameSetUp();
 
 
-// The Game (main function)
+
+
+// Step 3 - The Game (main function)
 document.onkeyup = function (event) {
-    userGuess = event.key;
+    var userPress = event.key.toLowerCase();
+    console.log(userPress)
+    // Call the Letter Validate function
+    letterValidate(userPress);
 
-    // Compare 
+    // Call the Result function
+    result();
 
+    letterGuessedText.textContent = letterGuessed.join(" ");
 
-    // 
-
-
-    // 
-
-    
 }
